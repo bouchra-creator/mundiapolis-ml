2661041e0e33df1cdc97cf08dc73e927e41b818e
bouchra-creator <78182781+bouchra-creator@users.noreply.github.com>
2021-03-25 23:30:05 +0100

    Update 0-neuron.py

--- 0x01-classification/0-neuron.py
+++ 0x01-classification/0-neuron.py
@@ -11,7 +11,7 @@ class Neuron:
   
         if not isinstance(nx, int):
             raise TypeError ('nx must be an integer')
-        if nx < 1:
+        elif nx < 1:
             raise ValueError('nx must be a positive integer')
         
         loc, scale = 0, 1
