4dcd78e39f5a71c34f24e95b62fb0bc15c635a83
bouchra-creator <78182781+bouchra-creator@users.noreply.github.com>
2021-03-26 16:13:53 +0100

    Add files via upload

---
+++ 0x01-classification/1-neuron.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import numpy as np 
+class Neuron:
+
+ def __init__(self,nx):
+
+        if not isinstance (nx,int):
+            raise TypeError ('nx must be an integer')
+
+        if nx < 1:
+            raise ValueError ('nx must be a positive integer')
+            self.W=np.random.normal(0,1,(1,nx))
+            self.b=0
+            self.A=0
+
+ @property
+ def get_W(self):
+     return self.__W
+
+@property
+def get_b(self):
+     return self.__b
+
+@property
+def get_A(self):
+     return self.__A
+
+@property   
+def get_W(self,w):
+      self.__W = W
+
+@property
+def get_b(self,b):
+      self.__b = b
+
+@property
+def get_A(self,A):
+      self.__A = A
+    
+
+
+
+    
