811cdb154fcc405d2a4d5b5184ead9cbef666183
bouchra-creator <78182781+bouchra-creator@users.noreply.github.com>
2021-03-26 20:26:57 +0100

    Update 6-neuron.py

--- 0x01-classification/6-neuron.py
+++ 0x01-classification/6-neuron.py
@@ -19,37 +19,37 @@ class Neuron:
         self.A = 0
 
  @property
- def W(self):
+  def W(self):
      return self.__W
 
 @property
-def b(self):
+  def b(self):
      return self.__b
 
 @property
-def A(self):
+  def A(self):
      return self.__A
 
 
-def forward_prop(self, X):
+ def forward_prop(self, X):
     a1 = np.matmul(self.__W, X) + self.__b
     self.__A = 1 /(1 + np.exp(-a1))
     return self.__A
 
-def cost(self, Y, A):
+ def cost(self, Y, A):
     m = Y.shape[1]
     cost =  Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
     sigma = np.sum(cost)
     cost =  -(1 / m)*sigma
     return cost
  
-def evaluate(self, X, Y):
+ def evaluate(self, X, Y):
     self.forward_prop(X)
     predict = np.where(self.__A >= 0.5, 1, 0)
     cost = self.cost(Y, self.__A)
     return predict, cost
 
-def gradient_descent(self, X, Y, A, alpha=0.05):
+ def gradient_descent(self, X, Y, A, alpha=0.05):
         
         dz = A - Y
         dw = np.matmul(X, dz.T) / A.shape[1]
@@ -57,7 +57,7 @@ def gradient_descent(self, X, Y, A, alpha=0.05):
         self.__W = self.__W - alpha * dw.T
         self.__b = self.__b - alpha * db
 
-def train(self, X, Y, iterations=5000, alpha=0.05):
+ def train(self, X, Y, iterations=5000, alpha=0.05):
         if type(iterations) is not int:
             raise TypeError("iterations must be an integer")
         if iterations <= 0:
