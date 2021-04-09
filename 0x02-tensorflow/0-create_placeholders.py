423cf129c336969e450e7da88a8369d9aba68873
bouchra-creator <78182781+bouchra-creator@users.noreply.github.com>
2021-04-09 22:08:23 +0100

    Update 0-create_placeholders.py

--- 0x02-tensorflow/0-create_placeholders.py
+++ 0x02-tensorflow/0-create_placeholders.py
@@ -4,5 +4,7 @@ import tensorflow as tf
 
 
 def create_placeholders(nx, classes):
-  
-  return x,y
+   
+    x = tf.placeholder(tf.float32, shape=(None, nx), name='x')
+    y = tf.placeholder(tf.float32, shape=(None, classes), name='y')
+    return x, y
